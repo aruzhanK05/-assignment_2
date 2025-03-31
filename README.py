4bbefe6c24289155d7843331a35feac6f41504d2
Sabimabi <rukulo.0303@gmail.com>
2025-03-31 18:13:58 +0500

    Add files via upload

---
+++ README.py
@@ -0,0 +1,23 @@
+#Crypto News Scraper 
+# WORK WAS MADE BY RAKHAT SABINA and KOZHAKHMET ARUZHAN
+''''
+Description
+This script fetches the latest cryptocurrency news from CoinGecko,
+extracts relevant articles, and saves them as a CSV file for easy reference.
+Users can filter news by keywords.
+
+Required Libraries: pip install requests beautifulsoup4 
+
+Each document contains additional  function definitions.
+INSTRUCTIONS
+
+Create a folder  crypto_news_scraper and locate .py files as: 
+__init__.py
+extract_news.py
+fetch_html.py
+save_to_csv.py
+ Make sure to locate main.py outside the folder.
+
+ 
+
+ '''
